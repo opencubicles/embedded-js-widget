31373b64d20f5351ec42c6224492dd1fc886e503
Amit <amit@opencubicles.com>
2016-09-09 11:13:59 +0000

    Initial Commit

---
+++ app/app.js
@@ -0,0 +1,41 @@
+define(['jquery', 'ractive', 'rv!templates/template', 'text!css/my-widget_embed.css'], function ($, Ractive, mainTemplate, css) {
+
+  'use strict';
+  
+  var app = {
+    init: function () {
+
+    var $style = $("<style></style>", {type: "text/css"});
+    $style.text(css);
+    $("head").append($style);
+    
+      // render our main view
+      this.ractive = new Ractive({
+        el: 'myWidget',
+        template: mainTemplate,
+        data: {
+          cnt: 0,
+          ts: 'never',
+        }
+      });
+      this.ractive.on({
+        mwClick: function(ev) {
+          ev.original.preventDefault()
+          this.set('cnt', this.get('cnt') + 1);
+          var that = this;
+          $.ajax({
+            url: "http://date.jsontest.com/",
+            dataType: "jsonp"
+          }).then(function(resp) {
+            that.set("ts", resp.time);
+          }, function(resp) {
+            that.set("ts", "Something bad happened");
+          });
+        }
+      });
+    }
+  };
+
+  return app;
+
+});
---
+++ config.js
@@ -0,0 +1,9 @@
+var requirejs = {
+    paths: {
+        text : "bower_components/text/text",
+        jquery: "bower_components/jQuery/dist/jquery",
+        ractive: "bower_components/ractive/ractive",
+        'amd-loader': "bower_components/rv/vendor/amd-loader",
+        rv: "bower_components/rv/rv",
+    }
+};
